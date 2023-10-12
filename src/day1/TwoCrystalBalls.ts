8de142f46c7759f70f1a1fc87397bce76d87ffce
Vladimir Bredikhin <brutal.v4@gmail.com>
2023-10-12 13:11:32 +0300

    day1: add TwoCrystalBalls implementation

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,18 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    const jumpAmount = Math.floor(Math.sqrt(breaks.length));
 
+    let i = jumpAmount;
+    for (; i < breaks.length; i += jumpAmount) {
+        if (breaks[i]) {
+            break;
+        }
+    }
+
+    for (let j = i - jumpAmount; j < i; j++) {
+        if (breaks[j]) {
+            return j;
+        }
+    }
+
+    return -1;
 }
