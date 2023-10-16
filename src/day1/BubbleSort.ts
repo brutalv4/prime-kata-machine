8b9205ce5946ce84038f1e733b7ef9bc3291c6c4
Vladimir Bredikhin <brutal.v4@gmail.com>
2023-10-16 14:35:34 +0300

    day1: add BubbleSort implementation

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,13 @@
 export default function bubble_sort(arr: number[]): void {
+    for (let i = arr.length; i > 0; i--) {
+        for (let j = 1; j < i; j++) {
+            if (arr[j - 1] <= arr[j]) {
+                continue;
+            }
 
+            const tmp = arr[j - 1];
+            arr[j - 1] = arr[j];
+            arr[j] = tmp;
+        }
+    }
 }
